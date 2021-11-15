81c73958b5747618db0c9561f622a7f3fefa5161
ratexbg <83062383+ratexbg@users.noreply.github.com>
2021-11-15 18:19:47 +0200

    Initial commit

---
+++ ForCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class ForCounter
+    {
+        static void Main(string[] args) 
+        {
+            for (int counter = 1; counter <= 10; counter++)
+            {
+                Console.WriteLine("{0}",counter);
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ WhileCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class WhileCounter
+    {
+        //static void Main(string[] args)
+        //{
+        //    int counter = 1;
+        //    while (counter<= 10)
+        //    {
+        //        Console.WriteLine( "{0}", counter);
+        //        ++counter;
+        //    }
+        //    Console.WriteLine() ;
+
+        //}
+    }
+}
